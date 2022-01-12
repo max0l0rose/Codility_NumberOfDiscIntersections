7b496d92c912ee624b0b8877fd8de2a4b85d20a8
9956799@gmail.com <Qqq_12345>
2022-01-12 21:54:05 +0200

    56

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(new int[] {1,2,1}));
+
+        System.out.println(solution.solution(new int[] {1, 2_147_483_647, 0}));
+
+        //        System.out.println(solution.solution(new int[] {1,1,1}));
+//        System.out.println(solution.solution(new int[] {1,0,1}));
+//        System.out.println(solution.solution(new int[] {1,0,0}));
+//        System.out.println(solution.solution(new int[] {0,0,0}));
+
 //        System.out.println(solution.solution(new int[] {2,1,0}));
 //        System.out.println(solution.solution(new int[] {1,5,2,1,4,0}));
     }
--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,47 @@
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
+
+	class P {
+		P(long x, byte begin) {
+			this.x = x;
+			this.begin = begin;
+		}
+		long x;
+		byte begin;
+	}
 
 	public int solution(int[] A) {
 		//Arrays.sort(A);
 
-		ArrayList<Map.Entry<Integer, Integer>> list = new ArrayList<>();
+		P[] arr = new P[A.length*2];
 
-		for (int i = 0; i <A.length ; i++)
-			list.add(new AbstractMap.SimpleImmutableEntry (i-A[i], A[i]));
-
-		list.sort(Comparator.comparingInt(Map.Entry::getKey));
-
-		ArrayList<AtomicInteger> arr = new ArrayList<>();
-
-		int c = 0;
-		int idxOld = list.get(0).getKey();
-		for (int i = 0; i < list.size(); i++) {
-			c += arr.size();
-			int dx = list.get(i).getKey() - idxOld;
-			idxOld = list.get(i).getKey();
-			for (int j = 0; j < arr.size(); j++) {
-				if (arr.get(j).addAndGet(-dx)<=0)
-					arr.remove(j--);
-			}
-			//if (A[i] > 0)
-			arr.add(new AtomicInteger(list.get(i).getValue()));
+		//int p = 0;
+		for (int i = 0; i<A.length; i++) {
+			arr[i*2] = new P(i - (long)A[i], (byte)1);
+			arr[i*2+1] = new P(i + (long)A[i], (byte)0);
 		}
 
+		Arrays.sort(arr,
+			(a, b) -> {
+				int c = Long.compare(a.x, b.x);
+				if (c!=0)
+					return c;
+				c = Byte.compare(b.begin, a.begin);
+				return c;
+			}
+		);
 
-		return c;
+		int c = 0;
+		int intersec = 0;
+		for (int i = 0; i < arr.length; i++)
+			if (arr[i].begin == 1)
+				if ((intersec += c++) > 10_000_000)
+					return -1;
+			else
+				c--;
+
+		return intersec;
 	}
 
 }
